69f90d468be1eb9375c7a92b1313167ee6f435b8
Shivam Bhattacharjee <shivam.bhattacharjee94@gmail.com>
2021-11-03 18:26:59 +0530

    pattern match route handelrs

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,10 +2,17 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> rewrite_path 
     |> log
     |> route
     |> format_response
   end
+
+  def rewrite_path( %{path: "/wildlife"} = conv) do
+    %{conv | path: "/wildthings"}
+  end
+
+  def rewrite_path(conv), do: conv
 
   def log(conv), do: IO.inspect conv
 
@@ -16,25 +23,59 @@ defmodule Servy.Handler do
       |> List.first
       |> String.split(" ")
 
-    %{ method: method, path: path, resp_body: "" }
+    %{ method: method, 
+      path: path, 
+      resp_body: "" ,
+      status: nil
+    }
   end
 
   def route(conv) do
-    if conv.path == "/wildthings" do
-      %{conv | resp_body: "Bears, Lions, Tigers"}
-    else 
-      %{conv | resp_body: "Teddy, Smokey, Kuma"}
-    end
+    route(conv, conv.method, conv.path)
+  end
+
+  def route(conv, "GET", "/wildthings") do
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def route(conv, "GET", "/bears") do
+    %{conv | status: 200, resp_body: "Teddy, Smokey, Kuma"}
+  end
+
+  def route(conv, "GET", "/bears/" <> id) do
+    %{ conv | status: 200, resp_body: "Bear #{id}" }
+  end
+
+  def route(conv, "DELETE", "/bears/" <> id) do
+    %{conv | status: 200, resp_body: "Delete Bear #{id}"}
+  end
+
+  def route(conv, _method, path) do
+    %{conv | status: 404, resp_body: "No #{path} here"}
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
     #{conv.resp_body}
     """
+  end
+
+  # private func, can only be called within defining module
+  # key of map is not an atom, but a number
+  # hence the => syntax is used
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
 
 
@@ -52,7 +93,51 @@ response = Servy.Handler.handle(request)
 IO.puts response
 
 request = """
+GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+request = """
 GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+request = """
+DELETE /bears/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
