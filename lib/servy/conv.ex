75dc1ecae0d63babf0a4c1eda0c0adf2efab6346
Shivam Bhattacharjee <shivam.bhattacharjee94@gmail.com>
2021-11-20 14:58:06 +0530

    change conv to struct and organise

---
+++ lib/servy/conv.ex
@@ -0,0 +1,24 @@
+defmodule Servy.Conv do
+  defstruct [method: "", path: "", resp_body: "", status: nil]
+
+  def full_status(conv) do
+    "#{conv.status} #{status_reason(conv.status)}"
+  end
+
+  @doc """
+  private func, can only be called within defining module
+  key of map is not an atom, but a number
+  hence the => syntax is used
+  """
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
+
+end
--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -10,6 +10,8 @@ defmodule Servy.Handler do
   import Servy.Parser, only: [parse: 1]
   import Servy.FileHandler, only: [handle_file: 2]
 
+  alias Servy.Conv
+
   @doc """
   Transforms request into response
   """
@@ -25,37 +27,37 @@ defmodule Servy.Handler do
   end
 
  
-  def route(%{ method: "GET", path: "/wildthings" } = conv) do
+  def route(%Conv{ method: "GET", path: "/wildthings" } = conv) do
     %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
-  def route(%{ method: "GET", path: "/about" } = conv) do
+  def route(%Conv{ method: "GET", path: "/about" } = conv) do
     @pages_path
       |> Path.join("about.html")
       |> File.read
       |> handle_file(conv)
   end
   
-  def route(%{ method: "GET", path: "/bears" } = conv) do
+  def route(%Conv{ method: "GET", path: "/bears" } = conv) do
     %{conv | status: 200, resp_body: "Teddy, Smokey, Kuma"}
   end
 
-  def route(%{ method: "GET", path: "/bears/" <> id } = conv) do
+  def route(%Conv{ method: "GET", path: "/bears/" <> id } = conv) do
     %{ conv | status: 200, resp_body: "Bear #{id}" }
   end
 
-  def route(%{ method: "DELETE", path: "/bears/" <> id } = conv ) do
+  def route(%Conv{ method: "DELETE", path: "/bears/" <> id } = conv ) do
     %{conv | status: 200, resp_body: "Delete Bear #{id}"}
   end
   
 
-  def route(%{ path: path } = conv) do
+  def route(%Conv{ path: path } = conv) do
     %{conv | status: 404, resp_body: "No #{path} here"}
   end
 
-  def format_response(conv) do
+  def format_response(%Conv{} = conv) do
     """
-    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP/1.1 #{Conv.full_status(conv)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
@@ -63,19 +65,7 @@ defmodule Servy.Handler do
     """
   end
 
-  # private func, can only be called within defining module
-  # key of map is not an atom, but a number
-  # hence the => syntax is used
-  defp status_reason(code) do
-    %{
-      200 => "OK",
-      201 => "Created",
-      401 => "Unauthorized",
-      403 => "Forbidden",
-      404 => "Not Found",
-      500 => "Internal Server Error"
-    }[code]
-  end
+  
 
 
 end
